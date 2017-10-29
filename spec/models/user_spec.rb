4fe2252c9f95116ccbf8529f79f8679dcc8bd8f7
alkp <apasenko@gmail.com>
2017-10-29 11:24:03 +0300

    Add associations user and question/answer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "Check the author of the object" do
+    user = create( :user )
+    question = create( :question )
+    expect(user.author_of?(question)).to eq false
+    expect(User.new.author_of?(question)).to eq false
+    expect(question.user.author_of?(question)).to eq true
+  end
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:questions).dependent(:destroy) }
 end
