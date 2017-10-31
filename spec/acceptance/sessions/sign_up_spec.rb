59d8634aa37f2f0ced7edda0178a809fe2d0da4e
alkp <apasenko@gmail.com>
2017-10-31 12:31:32 +0300

    Add sessions acceptance tests

--- spec/acceptance/sessions/sign_up_spec.rb
+++ spec/acceptance/sessions/sign_up_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
-feature 'User log in', %q{
-  In order to be able ask a question
-  As an user
-  I want to be able to log in
-} do
+feature 'User sign up' do
   given(:user) { create(:user) }
 
-  scenario 'Registered user try to log in' do
+  scenario 'Non-Registered user try to sign in' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Authorized user try to sign up' do
     sign_in(user)
-    expect(page).to have_content 'Signed in successfully.'
+    visit new_user_registration_path
+    expect(page).to have_content 'You are already signed in.'
     expect(current_path).to eq root_path
   end
 
@@ -22,7 +29,7 @@ feature 'User log in', %q{
     expect(current_path).to eq new_user_session_path
   end
 
-  scenario 'Authorized user try to log in' do
+  scenario 'Authorized user try to sign up' do
     sign_in(user)
     visit new_user_session_path
     expect(page).to have_content 'You are already signed in.'
